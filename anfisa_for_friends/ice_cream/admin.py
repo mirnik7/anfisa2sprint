3337acf65a4836f93159d8f2260cf0de1eb00c38
Miroslav Nikolaichenko <mirnik7thebest@yandex.com>
2024-07-17 22:57:00 +0300

    6

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Из модуля models импортируем модель Category...
-from .models import Category
+from .models import Category, Topping, Wrapper, IceCream
 
 # ...и регистрируем её в админке:
 admin.site.register(Category)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.register(IceCream)
