3600faaeba5d6a7504bfc97c42f98fcc05fa8ed0
Miroslav Nikolaichenko <mirnik7thebest@yandex.com>
2024-07-20 20:25:42 +0300

    2

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -5,8 +5,15 @@ from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    # Запрос:
-    ice_cream_list = IceCream.objects.values('id', 'title')
+    # Заключаем вызов методов в скобки
+    # (это стандартный способ переноса длинных строк в Python);
+    # каждый вызов пишем с новой строки, так проще читать код:
+    ice_cream_list = IceCream.objects.values(
+            'id', 'title', 'description'
+        # Верни только те объекты, у которых в поле is_on_main указано True:
+        # аналогичная запись - (is_on_main__exact=True)
+        # можно использовать exclude - противоположность filter
+        ).filter(is_on_main=True)
     # Полученный из БД QuerySet передаём в словарь контекста:
     context = {
         'ice_cream_list': ice_cream_list,
